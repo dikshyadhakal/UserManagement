e5b5c9656599672811fab27b15e4d1ad24bf4e09
dikshyadhakal <dikshya.779406@sdc.tu.edu.npgit config --global user.password macbookD@1212>
2025-03-04 14:58:52 +0545

    develop user added

--- UserManagement.Domain/Entities/User.cs
+++ UserManagement.Domain/Entities/User.cs
@@ -1,14 +1,14 @@
-﻿namespace UserManagement.Domain.Entities.User
-{ 
-public class User
-{
-    public int Id { get; set; }
-    public string? FirstName { get; set; }
-    public string ?LastName { get; set; }
-    public string ?Email { get; set; }
-    public int ContactNumber { get; set; }
+﻿//namespace UserManagement.Domain.Entities 
+//{ 
+//public class User
+//{
+//    public int Id { get; set; }
+//    public string? FirstName { get; set; }
+//    public string ?LastName { get; set; }
+//    public string ?Email { get; set; }
+//    public int ContactNumber { get; set; }
 
-    public ICollection<Product> Products { get; set; }
-}
-}
+//    //public ICollection<Product> Products { get; set; }
+//}
+//}
 
---
+++ UserManagement.Domain/Entities/User1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Domain.Entities
+{
+    public class User1
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public int ContactNumber { get; set; }
+
+        //public ICollection<Product> Products { get; set; }
+    }
+}
+    
